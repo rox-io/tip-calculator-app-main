11b81e9b9ae63c4bd49208a6378cf4e2749e0cf6
superduperocks <rox_prawl@icloud.com>
2021-11-16 14:12:57 -0500

    fixed html accessibility issues

--- script.js
+++ script.js
@@ -1,114 +1,114 @@
 //INPUTS SELECTION
-const billInput = document.querySelector('#bill-input');
+const billInput = document.querySelector('#bill');
 const tipBtns = document.querySelectorAll('#tip-wrapper button');
 const customInput = document.querySelector('#input-section-b input');
-const peopleInput = document.querySelector('#people-input');
+const peopleInput = document.querySelector('#people');
+const tipDisplay = document.querySelector('#tip');
+const totalDisplay = document.querySelector('#total');
+const invalidText = document.querySelector('#input-section-c .invalid-text');
+const resetBtn = document.querySelector('#reset-btn');
 
-//ITERATE OVER TIP BUTTONS TO ADD EVENT LISTENER ON EACH ONE
-for(let btn of tipBtns) {
-    btn.addEventListener('click', (e) => {
-        //IF BUTTON CLICKED CHECK ELEMENT VALUES
-        CheckValue(btn);
-    })
-}
+//VARIABLES
+let peopleInputValue = 0;
+let tipPerc = 0;
+let split;
+let totalTip;
+let totalPerPerson;
 
-//ADD EVENT LISTENER ON CUSTOM INPUT
-customInput.addEventListener('keydown', (e) => {
-    //IF ENTER KEY IS PRESSED
-    if(e.key === "Enter") {
-        //IF ELEMENT VALUE IS NOT VALID RESET OUTPUT DISPLAY
-        if(customInput.value === "" && peopleInput.value > 0) {
-            tipDisplay.textContent = '$0.00';
-            totalDisplay.textContent = '$0.00';
-            removeInvalidity();
+//EVENTS
+billInput.addEventListener('input', (e) => {
+    if(Number.isInteger(parseFloat(e.target.value)) && parseFloat(e.target.value) !== 0) {
+        if(peopleInputValue !== 0) {
+            calculateBill();
+            resetBtn.style="background-color: hsl(172, 67%, 45%)";
         }
         else {
-            //IF ENTER KEY IS PRESSED CALL FUNCTION
-            CheckValue(customInput);
+            invalid();
+        }
+    }
+    else {
+    }
+})
+
+for(let btn of tipBtns) {
+    btn.addEventListener('click', (e) => {
+        focusedBtn(tipBtns, e.target);
+        tipPerc = parseFloat(e.target.value);
+        if(peopleInputValue !== 0) {
+            calculateBill();
+        }
+        else {
+            invalid();
+        }
+    })  
+}
+
+customInput.addEventListener('input', (e) => {
+    removeFocus(tipBtns);
+    tipPerc = parseFloat(e.target.value);
+    if(Number.isInteger(parseFloat(e.target.value)) && parseFloat(e.target.value) !== 0) {
+        if(peopleInputValue !== 0) {
+            calculateBill();
+        }
+        else {
+            invalid();
         }
     }
 })
 
-//VALIDATION CHECK FUNCTION
-function CheckValue(el) {
-    //IF BOTH ELEMENT VALUES ARE VALID
-    if(el.value !== "" && peopleInput.value > 0) {
-        //CALCULATE BILL
-        calcBill(el);
-        //REMOVE INVALID STYLYING FROM ELEMENT
-        removeInvalidity();
-    }//ANYTHING ELSE
-    else {
-        invalid();
-    }
-}
-
-//INPUT VALUE CALCULATION FUNCTION
-let billSplit;
-let selectNum;
-let totalTip;
-let totalPerPerson;
-function calcBill(el) {
-    //STORES ELEMENT VALUE TO VARIABLE
-    selectNum = el.value;
-    //CALCULATES HOW MUCH EACH PERSON PAYS FOR BILL
-    billSplit = billInput.value / peopleInput.value;
-    //CALCULATES THE TIP TOTAL EACH PERSON WILL PAY
-    totalTip = selectNum / 100 * billSplit;
-    //CALCULATES THE TOTAL AMOUNT EACH PERSON WILL PAY
-    totalPerPerson = billSplit + totalTip;
-    //IF BILL VALUE IS NOT VALID
-    if(billInput.value === "") {
-        tipDisplay.textContent = '$0.00';
-        totalDisplay.textContent = '$0.00';
+peopleInput.addEventListener('input', (e) => {
+    if(Number.isInteger(parseFloat(e.target.value)) && parseFloat(e.target.value) !== 0) {
+        removeInvalid();
+        peopleInputValue = parseFloat(e.target.value);
     }
     else {
-        //DISPLAY CALCULATIONS
-        displayOutput();
+        peopleInputValue = 0;
+    }
+})
+
+//FUNCTIONS
+function focusedBtn(tipBtn, active) {
+    for(let btn of tipBtn) {
+        btn.classList.remove("tip-btn-selected");
+    }
+    active.classList.add("tip-btn-selected");
+}
+
+function removeFocus(tipBtn) {
+    for(let btn of tipBtn) {
+        btn.classList.remove("tip-btn-selected");
     }
 }
 
-//DISPLAY OUTPUT FUNCTION
-const tipDisplay = document.querySelector('#tip');
-const totalDisplay = document.querySelector('#total');
-function displayOutput() {
-    //DISPLAYS ROUNDED UP CALCULATION TOTALS IN THE OUTPUT ELEMENTS 
-    tipDisplay.textContent = "$" + (Math.round(totalTip * 100) / 100).toFixed(2);
-    totalDisplay.textContent = "$" + (Math.round(totalPerPerson * 100) / 100).toFixed(2);
-    //CHANGES RESET BUTTON BACKGROUND COLOR
-    resetBtn.style="background-color: hsl(172, 67%, 45%);"
+function calculateBill() {
+    split = parseFloat(billInput.value) / parseFloat(peopleInput.value);
+    totalTip = tipPerc / 100 * split;
+    totalPerPerson = totalTip + split;
+    tipDisplay.textContent =  "$" + (Math.round(totalTip * 100) / 100).toFixed(2);
+    totalDisplay.textContent =  "$" + (Math.round(totalPerPerson * 100) / 100).toFixed(2);
 }
 
-//INVALIDITY FUNCTION
-const invalidText = document.querySelector('#input-section-c .invalid-text');
 function invalid() {
-    //ADDS OUTLINE COLOR TO ELEMENT
-    peopleInput.classList.add('invalid-border');
-    //ADDS TEXT TO ELEMENT
+    peopleInput.classList.add('invalid-input-border');
     invalidText.classList.add('invalid-text');
     invalidText.textContent = "Can't be zero";
 }
 
-//REMOVE INVALIDITY FUNCTION
-function removeInvalidity() {
-    //REMOVES OUTLINE COLOR STYLE FROM ELEMENT
-    peopleInput.classList.remove('invalid-border');
-    //REMOVES TEXT FROM ELEMENT
-    invalidText.textContent = "";
+function removeInvalid() {
+    peopleInput.classList.remove('invalid-input-border');
     invalidText.classList.remove('invalid-text');
+    invalidText.textContent = "";
 }
 
-//RESET BUTTON CLICK EVENT
-const resetBtn = document.querySelector('#reset-btn');
 resetBtn.addEventListener('click', (e) => {
-    //RESETS INPUT VALUES
     billInput.value = "";
     customInput.value = "";
     peopleInput.value = "";
-    //RESETS TIP AND TOTAL ELEMENTS 
+    peopleInputValue = 0;
+    tipPerc = 0;
     tipDisplay.textContent = '$0.00';
     totalDisplay.textContent = '$0.00';
-    //CHANGES RESET BUTTON BACKGROUND COLOR TO DEFAULT 
-    resetBtn.style="background-color: hsl(183, 43%, 24%)";
-    removeInvalidity();
+    e.target.style="background-color: hsl(183, 43%, 24%)";
+    removeInvalid();
+    removeFocus(tipBtns);
 })
